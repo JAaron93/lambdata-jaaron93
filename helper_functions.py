2099b92bbb0cb5de2694b1c559390230fef20ffd
joshuaelamin <81278002+joshuaelamin@users.noreply.github.com>
2021-05-04 15:47:13 -0700

    Creating a new .py file to practive pep8 standards while also applying those very same standards to my helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -5,20 +5,32 @@ import numpy as np
 import pytest
 from sklearn.model_selection import train_test_split
 
+
 def null_count(df):
-    """This function will return the number of null values contained within a DataFrame"""
+    """This function will return the number of null values contained
+    within a DataFrame"""
     return df.isnull().sum().sum()
 
 
 def train_test_split(df, frac):
-    """Create a Train/Test split function for a dataframe and returns both the Training and Testing sets.""" 
-    """Frac referes to the precent of data you would like to set aside for training."""
+    """Create a Train/Test split function for a dataframe and returns both
+    the Training and Testing sets."""
+    """Frac referes to the precent of data you would like to set aside for
+     training."""
     train, test = train_test_split(
                                    df,
                                    train_size=frac,
-                                   #test_size=.20, 
                                    random_state=72
                                    )
-                                   
+
     return (train, test)
+
+
+def abbr_2_st(state_series, abbr_2_st=True):
+    """Return a new column with the full name from a State abbreviation column ->
+    An input of FL would return Florida. This function should also take a
+    boolean (abbr_2_state) and when False takes full state names and
+    return state abbreviations. -> An input of Florida would return Fl."""
+
+
 
