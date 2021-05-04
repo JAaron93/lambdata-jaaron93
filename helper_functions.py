d397e2f8491f4432975bd65083cf53da7c111a93
joshuaelamin <81278002+joshuaelamin@users.noreply.github.com>
2021-05-03 23:51:43 -0700

    Created two helper functions

---
+++ helper_functions.py
@@ -0,0 +1,29 @@
+"""Lambdata - Collection of Data Science Helper Functions"""
+
+import pandas as pd
+import numpy as np
+import random
+import math
+import pytest
+from sklearn.model_selection import train_test_split
+
+def null_count(df):
+"""This function will return the number of null values contained within a DataFrame"""
+    null_sum = df.isnull().sum().sum()
+
+    return null_sum
+
+
+def train_test_split(df, frac):
+"""Create a Train/Test split function for a dataframe and returns both the Training and Testing sets. 
+Frac referes to the precent of data you would like to set aside for training."""
+    frac = .80
+    train, test = train_test_split(
+                                   df,
+                                   train_size=frac,
+                                   test_size=.20,
+                                   shuffle=True, 
+                                   random_state=72
+                                   )
+                                   
+    return (train, test)
