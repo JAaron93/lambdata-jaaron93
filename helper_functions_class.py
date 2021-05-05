211815b618bb8239ad5c0ce184b2a8c15147ce3c
joshuaelamin <81278002+joshuaelamin@users.noreply.github.com>
2021-05-04 23:59:40 -0700

    adding classes to helper functions

---
+++ helper_functions_class.py
@@ -0,0 +1,35 @@
+"""Lambdata - Collection of Data Science Helper Functions"""
+
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+
+# Creating a Class
+
+
+class CleanData:
+    def__init__(self):
+        """
+        Init function for instantiation of objects
+        """
+        return
+
+
+# Confirms whether or not a DataFrame contains missing values
+    def null_count(df):
+        """
+        This function will return the number of null values contained
+        within a DataFrame
+        """
+        return df.isnull().sum().sum()
+
+    # Randomizer
+    def randomize(df, seed):
+        """
+        What it does is stated by the comment
+        """
+        randomized = shuffle(
+                             df,
+                             random_state=seed
+                             )
+        return randomized
