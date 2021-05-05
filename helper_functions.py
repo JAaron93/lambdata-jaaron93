95c9b5c953afc052048e08cfe59b4e04fd47e70a
joshuaelamin <81278002+joshuaelamin@users.noreply.github.com>
2021-05-04 20:46:15 -0700

    last commit actually worsened my grade

--- helper_functions.py
+++ helper_functions.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 from sklearn.model_selection import train_test_split
 
+
 # Confirms whether or not a DataFrame contains missing values
 def null_count(df):
     """This function will return the number of null values contained
@@ -18,23 +19,11 @@ def train_test_split(df, frac):
     the Training and Testing sets."""
     """Frac referes to the precent of data you would like to set aside for
      training."""
-    train, test = train_test_split(
-                                   df,
-                                   train_size=frac
-                                   )
+    arr_random = np.random.rand(df.shape[0])    #Random arrangement of rows
+    split = arr_random < np.percentile(arr_random, (frac*100))     #Percentile cutoff for split
+    df_testing = df[split]   # Upper Half of split
+    df_training = df[~split]   # Lower Half of split
 
-    return train, test
-
-
-# Randomized function that randomizes all of a DataFrames cells before returning it
-def randomize(df, seed):
-    df = df.sample(
-                   frac = 1,
-                   axis = 1,
-                   random_state = 42).sample(
-                                            frac = 1,
-                                            random_state = 59
-                                            ).reset_index(drop = True)
-    return df
+    return df_training, df_testing
 
 
